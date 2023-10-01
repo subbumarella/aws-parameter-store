7a1bb70a15f6cdd47bdf348dd5bb2e497a8a45c8
subbumarella <54053065+subbumarella@users.noreply.github.com>
2023-10-01 14:58:26 +0530

    added

---
+++ learn-ansible/learn-terraform/17.parameterstoreTerraform/main.tf
@@ -0,0 +1,17 @@
+resource "aws_ssm_parameter" "ex"{
+    count=length(var.params)
+    name=var.params[count.index].name
+    type=var.params[count.index].type
+    value=var.params[count.index].value
+}
+
+variable "params"{
+    default=[
+        {name="name1",type="String",value="value1"},
+        {name="name2",type="String",value="value2"},
+        {name="name3",type="String",value="value3"},
+        {name="name4",type="String",value="value4"},
+        {name="name5",type="String",value="value5"},
+        {name="name6",type="String",value="value6"}
+    ]
+}
